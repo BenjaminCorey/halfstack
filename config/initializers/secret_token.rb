1516ae264aa7f0ce81e36cb42e733adad446caa9
Benjamin Corey Wilkins <becowilk@gmail.com>
2012-12-04 19:01:59 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Halfstack::Application.config.secret_token = '6ed8c855c45e4726a81083c7f3021d99bb24e59697d7c098c175b90e409d29818e55074b6c76c6c42bdbd652d60bb1bcf1163cb46707f7814a2de443e791878d'
